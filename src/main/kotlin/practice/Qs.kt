94b51fd4335e254913666797fa5188140dae282a
Alun Davey <hey@4lun.io>
2018-06-05 15:28:34 +0100

    Completed question 2

--- src/main/kotlin/practice/Qs.kt
+++ src/main/kotlin/practice/Qs.kt
@@ -3,3 +3,5 @@ package practice
 fun manyTimesString(str: String, i: Int): String {
   return str.repeat(i)
 }
+
+data class SimpleTime(val hours: Int, val minutes: Int)
--- src/test/kotlin/test/PracticeTests.kt
+++ src/test/kotlin/test/PracticeTests.kt
@@ -9,4 +9,11 @@ class PracticeTests {
     val m1 = manyTimesString("abc", 3)
     assertEquals("abcabcabc", m1)
   }
+
+  @Test
+  fun q2() {
+    val t = SimpleTime(hours=5, minutes=30)
+    assertEquals(t.hours, 5)
+    assertEquals(t.minutes, 30)
+  }
 }
